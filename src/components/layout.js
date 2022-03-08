2a5ffe9e631610af9cadbef813fbc524891bf8b2
mehul-pawar <mehul.pawar@greylabs.com>
2022-03-08 19:31:12 +0530

    query for data

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,30 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link,useStaticQuery,graphql } from 'gatsby'
 import {
   container,
   heading,
   navLinks,
   navLinkItem,
-  navLinkText
+  navLinkText,
+  siteTitle,
 } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`)
   return (
+    
     <div className={container}>
+       <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+    <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+
       <title>{pageTitle}</title>
       <nav>
         <ul className={navLinks}>
