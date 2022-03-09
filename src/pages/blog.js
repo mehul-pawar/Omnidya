9428b31499d7dbc0cd7ae713f6bafe813ae0b6ec
mehul-pawar <mehul.pawar@greylabs.com>
2022-03-09 12:50:45 +0530

    Transfrom data to use MDX

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 const BlogPage = ({data}) => {
   return (
     <Layout pageTitle="My Blog Posts">
-     <ul>
+     {/* <ul>
        {
          data.allFile.nodes.map(node => (
           <li key={node.name}>
@@ -13,19 +14,46 @@ const BlogPage = ({data}) => {
           </li>
          ))
        }
-     </ul>
+     </ul> */}
+     {
+       data.allMdx.nodes.map(node=>(
+         <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>{node.frontmatter.date}</p>
+            <MDXRenderer>
+              {node.body}
+            </MDXRenderer>
+           </article>
+       ))
+     }
     </Layout>
   )
 }
 
-export const query = graphql`
+// export const query = graphql`
+// query{
+//   allFile {
+//     nodes {
+//       name
+//     }
+//   }
+// }
+// `
+
+export const query =graphql `
 query{
-  allFile {
+  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
     nodes {
-      name
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+      }
+      id
+      body
     }
   }
 }
+
 `
 
 export default BlogPage
