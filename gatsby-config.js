753c3ae0d92240062dd1a4e04ad569b683b1edcd
mehul-pawar <mehul.pawar@greylabs.com>
2022-03-09 11:54:33 +0530

    query for data with graphql1

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/Blog`,
+      }
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
